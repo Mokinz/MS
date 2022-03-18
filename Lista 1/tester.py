2e04ffa494f170e4081dd654b41d3f34298b7e5d
Mokinz <jz.jakubzolich@gmail.com>
2022-03-18 15:08:23 +0100

    zadanie 1 | 1/10

--- Lista 1/tester.py
+++ Lista 1/tester.py
@@ -1,10 +1,7 @@
 import numpy as np
 n = 1000
-wartosci = []
-for i in range(n):
-    temp = 0
-    for j in range(12):
-        temp = temp + np.random.rand()
-        wartosci.append(temp - 6)
 
-print(wartosci)
+p = list(np.random.rand(n))
+
+print(p)
+print(len(p))
